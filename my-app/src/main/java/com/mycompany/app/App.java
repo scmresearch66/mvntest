692deb7ca0402452e611606d6d1e51136a325035
Ralph Boerger <ralph.boerger@gmail.com>
2017-02-06 16:39:32 +0000

    added checkstyle

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -3,16 +3,13 @@ package com.mycompany.app;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class App 
-{
-    private static final Logger logger = LogManager.getLogger("App");
+public final class App {
+    private static final Logger THE_LOGGER = LogManager.getLogger("App");
 
-    private App() {}
+    private App() {
+    }
 
-    public static void main( String[] args )
-    {
-        logger.debug("This is an debug message");
-
-        /* System.out.println( "Hello World!" ); */
+    public static void main(final String[] args) {
+        THE_LOGGER.debug("This is an debug message");
     }
 }
