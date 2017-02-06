2b5309f1f8579e4dffa7cb86c9c92dbaca84eb40
Ralph Boerger <ralph.boerger@gmail.com>
2017-02-06 12:12:30 +0000

    fixing sonarqube

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,18 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class App 
 {
+    private static final Logger logger = LogManager.getLogger("App");
+
+    private App() {}
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        logger.debug("This is an debug message");
+
+        /* System.out.println( "Hello World!" ); */
     }
 }
